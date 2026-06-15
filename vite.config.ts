872d1898ef071c94bf28cd73e692f6f6f79d8fac
Fredrik Johansen <fredrik1206@gmail.com>
2026-06-15 10:07:02 +0200

    feat(vite): adds PWA plugin for the vite project

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({ registerType: "prompt", manifest: false }),
+  ],
   resolve: {
     alias: {
       "@shadcn-ui": path.resolve(__dirname, "./src/shadcn-ui"),
